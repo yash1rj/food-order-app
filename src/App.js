366702a000c9b3c0254d9583cf5a2973eb0baf6b
Yash Raj <yashrj130@gmail.com>
2021-05-25 18:50:01 +0530

    Update App.js
    -show/hide cart feature added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
+import Cart from './components/Cart/Cart';
 import Header from './components/Header/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
