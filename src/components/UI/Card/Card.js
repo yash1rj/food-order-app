e1c2d17ecd747681478c4e50feb3872d3c3d53a8
Yash Raj <yashrj130@gmail.com>
2021-05-25 18:15:39 +0530

    Update Card.js
    -applied class changed

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Card = props => {
     return (
-        <div className={`${classes.card} ${props.className}`}>
+        <div className={classes.card}>
             {props.children}
         </div>
     );
