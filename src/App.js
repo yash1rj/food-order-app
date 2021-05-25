aa2b5458a9f28b9a2b0d727fb3aa85ee88b81818
Yash Raj <yashrj130@gmail.com>
2021-05-25 18:14:22 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { Fragment } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
