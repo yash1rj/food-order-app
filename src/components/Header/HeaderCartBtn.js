68d7eebb4bc17e27882f110fb03e5b31d668ff05
Yash Raj <yashrj130@gmail.com>
2021-05-25 23:37:37 +0530

    Update HeaderCartBtn.js
    -rendering no. of cart items dynamically

--- src/components/Header/HeaderCartBtn.js
+++ src/components/Header/HeaderCartBtn.js
@@ -1,8 +1,16 @@
 import classes from './HeaderCartBtn.module.css';
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartBtn = props => {
+
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount
+    }, 0);
+
     return (
         <button onClick={props.onClick} className={classes.button}>
             <span className={classes.icon}>
@@ -12,7 +20,7 @@ const HeaderCartBtn = props => {
                 My Cart
             </span>
             <span className={classes.badge}>
-                2
+                {numberOfCartItems}
             </span>
         </button>
     );
