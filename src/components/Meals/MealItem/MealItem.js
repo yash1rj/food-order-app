238c93188504f6b58f0ff568ee77345b9d73d8fa
Yash Raj <yashrj130@gmail.com>
2021-05-25 23:36:24 +0530

    Update MealItem.js
    -adding obtained amount of item from mealItem form to cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,20 +1,32 @@
 import classes from './MealItem.module.css';
-import React from 'react';
+import React, { useContext } from 'react';
 import MealItemForm from './MealItemForm/MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = props => {
 
+    const cartCtx = useContext(CartContext);
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (enteredAmount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            price: props.price,
+            amount: enteredAmount
+        });
+    };
 
     return (
         <li className={classes.meal}>
             <div>
-                <h3>{props.title}</h3>
+                <h3>{props.name}</h3>
                 <div className={classes.description}>{props.desc}</div>
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
