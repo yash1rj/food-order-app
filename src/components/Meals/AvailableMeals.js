b827f9840d9b79705e62706b32fa42b406daf87a
Yash Raj <yashrj130@gmail.com>
2021-05-27 16:34:16 +0530

    Update AvailableMeals.js
    -removing dummy list of data
    -connecting to firebase db to get list od meals data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,40 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
 const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal => {
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const resp = await fetch('https://food-order-app-babae-default-rtdb.firebaseio.com/meals.json');
+            const respData = await resp.json();
+
+            const loadedMeals = [];
+
+            for (const key in respData) {
+                loadedMeals.push({ id: key, ...respData[key] });
+            }
+
+            setMeals(loadedMeals);
+            setIsLoading(false);
+        };
+
+        fetchMeals();
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
+    const mealsList = meals.map(meal => {
         return <MealItem id={meal.id} key={meal.id} name={meal.name} desc={meal.description} price={meal.price} />
     });
 
